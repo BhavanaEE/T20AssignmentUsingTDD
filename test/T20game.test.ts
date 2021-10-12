75a427988f819f9796b5b61c4638a1794147d86a
BhavanaEE <bhavana.chivukula@everest.engineering>
2021-10-12 12:32:32 +0530

    Added 2nd testcase for random number

--- test/T20game.test.ts
+++ test/T20game.test.ts
@@ -1,6 +1,15 @@
 import {swapPlayers} from "../src/T20game"
+import {randomFunction} from "../src/strategy"
+
 test("Swapping players",()=>{
     let received=swapPlayers("Nodhi","kirat");
     let expected=["kirat","Nodhi"]
     expect(received).toEqual(expected)
+})
+
+test("Runs for a player",()=>{
+    let expected=40
+    jest.spyOn(global.Math,"random").mockImplementation(()=>0.4)
+    let received=randomFunction();
+    expect(received).toBe(expected);
 })
