4731a573bd6349b30afed286360e1045b976312e
BhavanaEE <bhavana.chivukula@everest.engineering>
2021-10-12 16:18:18 +0530

    Added structure for player properties and initialized

--- src/T20game.ts
+++ src/T20game.ts
@@ -1,4 +1,29 @@
-export function swapPlayers(batsman:string,runner:string):string[]{
-    [batsman,runner]=[runner,batsman]
-    return [batsman,runner]
+import {scoreOfPlayer} from "../src/strategy"
+import {batsmanIndexGenerator} from "../src/strategy"
+
+let playersName:string[]=["Kirat","Nodhi","Rumrah","shashi"];
+let playerProperties:{
+    [playerName:string]:{
+        score:number,
+        noOfBallsPlayed:number,
+        out:boolean
+    }
+
+}={};
+
+export function swapPlayers(batsMan:string,runner:string){
+    [batsMan,runner]=[runner,batsMan]
+    return [batsMan,runner]
 }
+
+function playerInitialization(){
+    for(let i=0;i<4;i++){
+        playerProperties[playersName[i]]={
+            score:0,
+            noOfBallsPlayed:0,
+            out:false,
+        };
+    }
+
+}
+
