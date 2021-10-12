f8be258cb5172667ec4ce30567488f5e6aab4ae5
BhavanaEE <bhavana.chivukula@everest.engineering>
2021-10-12 17:20:18 +0530

    Added testcase - detect whether player is out or not

--- test/T20game.test.ts
+++ test/T20game.test.ts
@@ -1,7 +1,14 @@
 import {swapPlayers} from "../src/T20game"
+import {isOutOrNot} from "../src/T20game"
 
 test("Swapping players",()=>{
     let received=swapPlayers("Nodhi","Kirat");
     let expected=["Kirat","Nodhi"]
     expect(received).toEqual(expected)
+})
+
+it("Function has to detect whether player is out or not",()=>{
+    let expected=true
+    let received=isOutOrNot("Rumrah",7)
+    expect(received).toBe(expected)
 })
