dc8e46dec386ae9cae3204a9237946a637f6f659
BhavanaEE <bhavana.chivukula@everest.engineering>
2021-10-13 16:30:29 +0530

    Added score board and result of match functions

--- src/T20game.ts
+++ src/T20game.ts
@@ -43,6 +43,52 @@ export function setPlayerPropertyBasedOnScore(batsMan:string,scoreOutcome:number
     }
 }
 
+function scoreBoard(playerProperties:playerProperty,playersName:string[]){
+    for(let i=0;i<4;i++)
+    {
+        let nameOfPlayer:string=playersName[i];
+        let score:number=playerProperties[nameOfPlayer].score;
+        let balls:number=playerProperties[nameOfPlayer].noOfBallsPlayed;
+        if(playerProperties[nameOfPlayer].out==false){
+            if(balls===0)
+            console.log(`${nameOfPlayer} - ${score} (${balls} balls)`)
+            else
+            console.log(`${nameOfPlayer} - ${score}* (${balls} balls)`)
+        }
+        else        
+        console.log(`${nameOfPlayer} - ${score} (${balls} balls)`)
+    }
+}
+
+function resultOfMatch(hasWon:boolean,over:number,ballAtParticularOver:number,targetScore:number,teamScore:number){
+    if(hasWon && ballAtParticularOver)
+        {
+            let ballsRemaining:number=24-(((over-1)*6)+ballAtParticularOver)
+            if(ballsRemaining<=1)
+            console.log(`Bengaluru won by ${4-wickets} wickets and ${ballsRemaining} ball remaining`)
+            else
+            console.log(`Bengaluru won by ${4-wickets} wickets and ${ballsRemaining} balls remaining`)
+            scoreBoard(playerProperties,playersName);
+            return;
+        }
+        if(wickets===3 && !hasWon){
+            if((targetScore-teamScore)===0){
+                console.log("Draw Match");
+                scoreBoard(playerProperties,playersName);
+                return;
+            }
+            console.log(`Chennai has won by ${(targetScore-teamScore)} runs`);
+            scoreBoard(playerProperties,playersName);
+            return;
+        }
+    if((targetScore-teamScore)===0){
+        console.log("Draw Match");
+    }
+    else
+    console.log(`Chennai has won by ${(targetScore-teamScore)} runs`);
+    scoreBoard(playerProperties,playersName);
+}
+
 export function startGame(){
     let over:number,ballsInEachOver:number,ballAtParticularOver:number=0,targetScore:number=40,scoreOutcome:number;
     let hasWon=false;
@@ -75,4 +121,5 @@ export function startGame(){
         }
         [batsMan,runner]=swapPlayers(batsMan,runner);
     }
+    resultOfMatch(hasWon,over,ballAtParticularOver,targetScore,teamScore)
 }
