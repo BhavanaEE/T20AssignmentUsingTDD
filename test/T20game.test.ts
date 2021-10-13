526a65bde490c0cbe507312b20d6258e169c9fe5
BhavanaEE <bhavana.chivukula@everest.engineering>
2021-10-13 16:28:23 +0530

    Manually checking count of balls played by player

--- test/T20game.test.ts
+++ test/T20game.test.ts
@@ -24,3 +24,14 @@ it("Checking score of player when player hits six before and after a ball",()=>{
     let received=playerProperties["Nodhi"].score;
     expect(received).toBe(expected);
 })
+
+test("If Nodhi serves 4 balls,then count of balls(4) should be added to Nodhi",()=>{
+    let playerProperties=playerInitialization();
+    let expected=4
+    setPlayerPropertyBasedOnScore("Nodhi",6);
+    setPlayerPropertyBasedOnScore("Nodhi",2);
+    setPlayerPropertyBasedOnScore("Nodhi",2);
+    setPlayerPropertyBasedOnScore("Nodhi",4);
+    let received=playerProperties["Nodhi"].noOfBallsPlayed;
+    expect(received).toBe(expected);
+})
